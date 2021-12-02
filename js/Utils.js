6a95da8d81fd20fd1f76949410a255751a7cfa46
kanarian <45354444+kanarian@users.noreply.github.com>
2021-12-02 15:41:14 +0100

    Calculate average CO2 per party only over municips with data

--- js/Utils.js
+++ js/Utils.js
@@ -26,16 +26,20 @@ const calculateRenewableVSCO2 = (renewData, co2Data) => {
 const calculateCO2PerPoliticalParty = (electionData, co2Data) => {
   let unique_party_list = electionData.filter(entry =>  entry["municipality"] == "Nederland").map(entry => entry["party_name"])
   let party_avg_co2_value = []
+  let municipalities_with_co2Data = co2Data.filter(entry => entry["CO2"] > 0).map(entry => entry["municipality"])
+  let electionDataCorr = electionData.filter(entry => municipalities_with_co2Data.includes(entry["municipality"] ))
+  console.log(electionDataCorr)
   for (let i = 0; i < unique_party_list.length; i++){
     let party = unique_party_list[i]
-    let votes_party_list = electionData.filter(entry =>  entry["party_name"] == party).map(entry => isNaN(entry.votes) ? {...entry, "votes": 0}: entry)
+    let votes_party_list = electionDataCorr.filter(entry =>  entry["party_name"] == party).map(entry => isNaN(entry.votes) ? {...entry, "votes": 0}: entry)
+                      
     for(let j = 0; j < votes_party_list.length; j++){
       if (typeof votes_party_list[j].percentage == undefined){
         console.log(votes_party_list[j].percentage)
         console.log("undefined shit " +votes_party_list[j].party_name)
       }
     }
-    let total_votes = electionData.filter(entry =>  entry["municipality"] == "Nederland" && entry['party_name'] == party)[0].votes
+    let total_votes = electionDataCorr.filter(entry =>  entry['party_name'] == party).reduce((acc,curr) => {return acc + curr['votes']},0);
     let percentage_votes_per_party_list = votes_party_list.filter(entry => entry["municipality"] != "Nederland")
     let weighted_co2_value = 0
     for (entry of percentage_votes_per_party_list){
