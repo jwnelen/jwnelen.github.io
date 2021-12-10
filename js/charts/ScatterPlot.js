4b5edf3d351e382e352bdae43e0b019206287896
jwnelen <jwnelen@gmail.com>
2021-12-10 17:56:52 +0100

    added color of the climate

--- js/charts/ScatterPlot.js
+++ js/charts/ScatterPlot.js
@@ -1,5 +1,5 @@
 class ScatterPlot {
-  constructor(id, data, additional_data=[], keyX, keyY) {
+  constructor(id, data, additional_data = [], keyX, keyY) {
     this.margin = {top: 10, right: 30, bottom: 30, left: 60},
         this.width = 860 - this.margin.left - this.margin.right,
         this.height = 800 - this.margin.top - this.margin.bottom;
@@ -30,12 +30,16 @@ class ScatterPlot {
 
     const xs = this.data.map(d => d[this.keyX]).filter((a) => a > 0)
     const ys = this.data.map(d => d[this.keyY]).filter((a) => a > 0)
-
+    const climates_labels = this.additional_data.map(d => d['climate_label']).filter((a) => a > 0)
+    console.log(climates_labels)
     const minX = Math.min(...xs);
     const maxX = Math.max(...xs);
     const minY = Math.min(...ys)
     const maxY = Math.max(...ys)
-
+    const minClimate = Math.min(...climates_labels)
+    const maxClimate = Math.max(...climates_labels)
+    console.log(minClimate)
+    console.log(maxClimate)
     // Add X axis
     const xScale = d3.scaleLinear()
         .domain([minX, maxX])
@@ -53,6 +57,9 @@ class ScatterPlot {
     svg.append("g")
         .call(d3.axisLeft(yScale));
 
+    let myColor = d3.scaleLinear().domain([minClimate,maxClimate])
+        .range(["red", "green"])
+
     // Add dots
     const circles = svg.append('g')
         .selectAll("dot")
@@ -63,8 +70,15 @@ class ScatterPlot {
         .attr("cy", d => yScale(d[self.keyY]))
         .attr("r", 3.5)
         .attr('municipality_name', (d) => d['municipality'])
-        .attr('climate_label', (d) => ("mun"))
-        .style("fill", "#69b3a2")
+        .attr('climate_label', (d) => {
+          const res = this.additional_data.filter((a) => a.municipality === d['municipality'])
+          return res.length > 0 ? res[0].climate_label : 0
+        })
+        .style("fill", (d) => {
+          const res = this.additional_data.filter((a) => a.municipality === d['municipality'])
+          const l = res.length > 0 ? res[0].climate_label : 0
+          return myColor(l)
+        })
 
     let tip = d3.select("body").append("div")
         .attr("class", "tooltip")
