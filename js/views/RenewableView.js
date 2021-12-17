bb606c6a7cfb64d08236c5cd18cdbc0360d3b530
Jeroen Nelen <jwnelen@gmail.com>
2021-12-17 15:35:31 +0100

    Fixed histogram

--- js/views/RenewableView.js
+++ js/views/RenewableView.js
@@ -7,12 +7,7 @@ class RenewableView extends View {
 
 	init() {
 		this.map = new GeoMap("map_renew", this.mapData, this, (e) => this.onMunClicked(e))
-		const munSelected = this.renewable.filter(m => m["municipality"] === state.selectedMunicipality)[0]
-		if (munSelected) {
-			const labels = Object.entries(munSelected).filter(([key, val], index) => key !== 'municipality')
-			const d = labels.map(([lab, va], index) => ({"label": lab, "value": va}))
-			this.barChart = new BarChart('barchartrenew', d, "label", "value", false)
-		}
+		this.histogram = new Histogram('histogram_renew', this.renewable, 'energy', null, (e) => this.onMunClicked(e))
 	}
 
 	highlightSelected = () => {
@@ -20,13 +15,8 @@ class RenewableView extends View {
 		this.map.update()
 		this.map.colorPath(mun_name);
 		$(".mun-name").html(mun_name);
-		const munSelected = this.renewable.filter(m => m["municipality"] === mun_name)[0]
+		this.histogram.update(this.renewable,null, mun_name)
 
-		if (munSelected) {
-			const labels = Object.entries(munSelected).filter(([key, val], index) => key !== 'municipality')
-			const d = labels.map(([lab, va], index) => ({"label": lab, "value": va}))
-			this.barChart.update(d)
-		}
 	}
 
 	onMunClicked = (d) => {
